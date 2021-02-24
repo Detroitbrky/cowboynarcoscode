42bef9a0f17c9b3e8733784ba83481707aab363b
Detroitbrky <79333386+Detroitbrky@users.noreply.github.com>
2021-02-24 14:11:27 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,5 +1,5 @@
 const {ShardingManager} = require ('discord.js');
-const manager = new ShardingManager ('./index.js', {totalShards: 'auto', token: 'ODE0MDcwMjQ1MjY2NDIzODM4.YDYf_w.XuwimmG3Rv8nmg8e9XbxAUClq9g'}); // TOKEN discord.com/developers Alabilirsiniz!
+const manager = new ShardingManager ('./index.js', {totalShards: 'auto', token: ''}); // TOKEN discord.com/developers Alabilirsiniz!
 
 manager.on ('shardCreate', shard => console.log (`Shardlar Başlatılıyor!`));
 manager.spawn ();
